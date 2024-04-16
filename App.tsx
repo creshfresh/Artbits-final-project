5b8394aaad36552949b3735c85fbad3aa8ee9636
Cristina Ramos <crdefrancisco@gmail.com>
2024-04-16 17:27:10 +0200

    at least i cant sign in with google in android

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import SignInScreen from "./app/screens/SignInScreen";
 import * as WebBrowser from "expo-web-browser";
 import {
@@ -10,21 +10,28 @@ import {
   signInWithCredential,
 } from "firebase/auth";
 
-import { useIdTokenAuthRequest } from "expo-auth-session/providers/google";
-
+import * as Google from "expo-auth-session/providers/google";
 import { auth } from "./firebaseConfig";
+import { makeRedirectUri } from "expo-auth-session";
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
   const [userInfo, setUserInfo] = useState();
-  const [request, response, promptAsync] = useIdTokenAuthRequest({
-    iosClientId: "",
+  // const discovery = {
+  //   authorizationEndpoint: "https://accounts.google.com/o/oauth2/v2/auth",
+  //   tokenEndpoint: "https://oauth2.googleapis.com/token",
+  //   revocationEndpoint: "https://oauth2.googleapis.com/revoke",
+  // };
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    expoClientId:
+      "1006799876952-fd27ptn97km3flsb8iimb42v7acquea2.apps.googleusercontent.com",
     webClientId:
       "1006799876952-tfrrji7mdatmj72e1o635kp20apf3don.apps.googleusercontent.com",
     androidClientId:
       "1006799876952-5jft6q2blgrgh64ptcd5a638mar38ihn.apps.googleusercontent.com",
   });
+
   useEffect(() => {
     if (response?.type === "success") {
       const { id_token } = response.params;
@@ -50,6 +57,3 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
-function UseState(): [any, any] {
-  throw new Error("Function not implemented.");
-}
