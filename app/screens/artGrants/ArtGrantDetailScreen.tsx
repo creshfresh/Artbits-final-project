6cebcdde20a3047019242da97bb99786dd81edea
Cristina Ramos <crdefrancisco@gmail.com>
2024-05-19 21:14:31 +0200

    wip que si no me lio

--- app/screens/artGrants/ArtGrantDetailScreen.tsx
+++ app/screens/artGrants/ArtGrantDetailScreen.tsx
@@ -10,12 +10,11 @@ import {
 import { usePersonStore } from "../../../store/store";
 import { useTranslation } from "../../hooks/useTranslations";
 import { colors } from "../../theme/colors";
-import { ArtGrantScreenControler } from "./ArtGrantScreenControler";
 import { useRoute } from "@react-navigation/native";
 const screenWidth = Dimensions.get("window").width;
-export const ArtGrantDetailScreen = ({}) => {
-  // no borrar
+const win = Dimensions.get("window");
 
+export const ArtGrantDetailScreen = ({}) => {
   const route: any = useRoute();
   //   const { name } = route.params;
   const user = usePersonStore((state) => state.user);
@@ -27,95 +26,129 @@ export const ArtGrantDetailScreen = ({}) => {
       style={{ backgroundColor: "transparent" }}
     >
       <View style={styles.container}>
+        <View style={{ flexDirection: "column", top: 40 }}>
+          <Image
+            source={{ uri: route.params.item.image[0] }}
+            style={[styles.image]}
+          />
+        </View>
         <View
-          style={{ flexDirection: "column", top: 20, marginHorizontal: 20 }}
+          style={{
+            position: "absolute",
+            borderColor: colors.secondary,
+            borderWidth: 2,
+            alignItems: "center",
+            padding: 3,
+            top: 180,
+            zIndex: 1,
+            left: 20,
+            paddingHorizontal: 12,
+            borderRadius: 30,
+          }}
         >
-          <Image source={{ uri: route.params.item.image[0] }} style={[styles.image]} />
+          <Text
+            style={[
+              styles.body,
+              { color: colors.secondary, fontWeight: "700", fontSize: 10 },
+            ]}
+          >
+            {route.params.item.participants}
+          </Text>
+        </View>
+        <View
+          style={[styles.loginCard, { shadowColor: "#000000", elevation: 20 }]}
+        >
           <View
             style={{
-              marginTop: 100,
               flexDirection: "column",
               justifyContent: "center",
               alignContent: "center",
             }}
           >
             <View style={{ justifyContent: "center", alignItems: "center" }}>
-              <View style={{ padding: 10, flexDirection: "row" }}>
+              <View style={{ paddingTop: 40, flexDirection: "row" }}>
                 <Text style={styles.title}>{route.params.item.name}</Text>
               </View>
-              <View
-                style={{
-                  height: 2,
-                  backgroundColor: "#EBE9E9",
-                  marginVertical: 5,
-                }}
-              ></View>
-              <View
-                style={{
-                  flexDirection: "row",
-                  width: "100%",
-                  alignItems: "center",
-                }}
-              >
-                <View style={{ flex: 1, margin: 5 }}>
-                  <Text style={styles.subtitle}>{t("start.date")}</Text>
-                  <Text style={styles.body}>{route.params.item.name}</Text>
-                </View>
-                <View style={{ flex: 1, margin: 5 }}>
-                  <Text style={styles.subtitle}>{t("dead.line")}</Text>
-                  <Text style={styles.body}>{route.params.item.name}</Text>
-                </View>
+
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Text style={styles.body}>
+                  {route.params.item.organization}
+                </Text>
               </View>
+
               <View
                 style={{
                   flexDirection: "row",
-                  width: "100%",
+                  marginVertical: 10,
+                  gap: 30,
+                  justifyContent: "space-around",
                   alignItems: "center",
                 }}
               >
-                <View style={{ flex: 1, margin: 5 }}>
-                  <Text style={styles.subtitle}>{t("participants")}</Text>
-                  <Text style={styles.body}>
-                    {route.params.item.participants}
-                  </Text>
+                <View style={{ justifyContent: "center" }}>
+                  <Text style={styles.subtitle}>{t("start.date")}</Text>
+                  {/* Aqui tengo que coger la fecha formateada */}
+                  <Text style={styles.body}>19/05/2024</Text>
                 </View>
-                <View style={{ flex: 1, margin: 5 }}>
-                  <Text style={styles.subtitle}>{t("destinty.centre")}</Text>
-                  <Text style={styles.body}>
-                    {route.params.item.organization}
-                  </Text>
+                <View style={{ justifyContent: "center" }}>
+                  {/* Aqui tengo que coger la fecha formateada */}
+                  <Text style={styles.subtitle}>{t("dead.line")}</Text>
+                  <Text style={styles.body}>19/05/2024</Text>
                 </View>
               </View>
             </View>
           </View>
-
           <View
-            style={{ height: 2, backgroundColor: "#EBE9E9", marginVertical: 5 }}
+            style={{
+              height: 2,
+              backgroundColor: "#EBE9E9",
+              alignContent: "center",
+              left: 0,
+              marginVertical: 10,
+              marginHorizontal: -20,
+              width: win.width,
+            }}
           ></View>
-          <View style={{ gap: 10, paddingBottom: 20 }}>
+          <View style={{ gap: 10, paddingBottom: 15 }}>
             <Text style={styles.titleBody}>{t("destinty.centre")}</Text>
-            <Text style={styles.bodybody}>{route.params.item.terms}</Text>
+            <Text style={styles.bodybody}>
+              {route.params.item.destinyCentre}
+            </Text>
           </View>
-          <View style={{ gap: 10, paddingBottom: 20 }}>
-            <Text style={styles.titleBody}>{t("destinty.centre")}</Text>
-            <Text style={styles.bodybody}>{route.params.item.terms}</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              gap: 40,
+            }}
+          >
+            <View style={{ gap: 10, paddingBottom: 15 }}>
+              <Text style={styles.titleBody}>{t("min.age")}</Text>
+              <Text style={styles.bodybody}>{route.params.item.minAge}</Text>
+            </View>
+            <View style={{ gap: 10, paddingBottom: 15 }}>
+              <Text style={styles.titleBody}>{t("max.age")}</Text>
+              <Text style={styles.bodybody}>{route.params.item.maxAge}</Text>
+            </View>
           </View>
-          <View style={{ gap: 10, paddingBottom: 20 }}>
-            <Text style={styles.titleBody}>{t("min.age")}</Text>
-            <Text style={styles.bodybody}>{route.params.item.minAge}</Text>
+          <View style={{ gap: 10, paddingBottom: 15 }}>
+            <Text style={styles.titleBody}>{t("total.granted")}</Text>
+            <Text style={styles.bodybody}>
+              {route.params.item.totalGranted}
+            </Text>
           </View>
-          <View style={{ gap: 10, paddingBottom: 20 }}>
-            <Text style={styles.titleBody}>{t("max.age")}</Text>
-            <Text style={styles.bodybody}>{route.params.item.maxAge}</Text>
-          </View>
-          <View style={{ gap: 10, paddingBottom: 20 }}>
+          <View style={{ gap: 10, paddingBottom: 15 }}>
             <Text style={styles.titleBody}>{t("work.specifications")}</Text>
             <Text style={styles.bodybody}>
               {route.params.item.specifications}
             </Text>
           </View>
 
-          <View style={{ gap: 10, paddingBottom: 20 }}>
+          <View style={{ gap: 10, paddingBottom: 15 }}>
+            <Text style={styles.titleBody}>{t("terms")}</Text>
+            <Text style={styles.bodybody}>{route.params.item.terms}</Text>
+          </View>
+          <View style={{ gap: 10, paddingBottom: 140 }}>
             <Text style={styles.titleBody}>{t("web.url")}</Text>
             <Text
               style={styles.link}
@@ -126,13 +159,12 @@ export const ArtGrantDetailScreen = ({}) => {
               {route.params.item.weburl}
             </Text>
           </View>
-
-          <View style={{ gap: 10, paddingBottom: 100 }}>
+          {/* <View style={{ gap: 10, paddingBottom: 100 }}>
             <Text style={styles.titleBody}>{t("download.bases")}</Text>
             <Text style={styles.bodybody}>
               {route.params.item.urlbases.assets[0].uri}
             </Text>
-          </View>
+          </View> */}
         </View>
       </View>
     </ScrollView>
@@ -141,7 +173,8 @@ export const ArtGrantDetailScreen = ({}) => {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.background,
+    paddingTop: 50,
+    backgroundColor: "#D7E2F4",
   },
   division: {
     marginVertical: 5,
@@ -149,12 +182,22 @@ var styles = StyleSheet.create({
     width: screenWidth,
     alignItems: "center",
   },
-
   link: {
     fontSize: 14,
     letterSpacing: 1.5,
-    // textAlign:"center",
     color: colors.secondary,
+  },
+
+  loginCard: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: "white",
+    borderTopEndRadius: 30,
+    borderTopStartRadius: 30,
+    left: 0,
+    right: 0,
+    bottom: -100,
+    width: "100%",
   },
   title: {
     fontSize: 24,
@@ -163,7 +206,7 @@ var styles = StyleSheet.create({
     color: colors.text,
   },
   titleBody: {
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: "600",
     letterSpacing: 1.5,
     color: colors.text,
@@ -177,19 +220,18 @@ var styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: "500",
     letterSpacing: 1.5,
-    // textAlign:"center",
     color: colors.text,
   },
   bodybody: {
     fontSize: 14,
     letterSpacing: 1.5,
-    // textAlign:"center",
     color: colors.text,
   },
 
   image: {
     width: 100,
     height: 100,
+    zIndex: 1,
     borderColor: "white",
     backgroundColor: "#DEDEDE",
     borderWidth: 3,
